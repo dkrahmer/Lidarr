c9d9478f0334d65b4e9ea81ce6070e0cd019c5b6
Doug Krahmer <doug.git@remhark.com>
2020-10-12 13:06:36 -0700

    Remove "Disk Space" section on Status screen. Prevents waking up hard drives that may be sleeping.

--- frontend/src/System/Status/Status.js
+++ frontend/src/System/Status/Status.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import PageContent from 'Components/Page/PageContent';
 import PageContentBody from 'Components/Page/PageContentBody';
 import AboutConnector from './About/AboutConnector';
-import DiskSpaceConnector from './DiskSpace/DiskSpaceConnector';
 import HealthConnector from './Health/HealthConnector';
 import MoreInfo from './MoreInfo/MoreInfo';
 
@@ -16,7 +15,6 @@ class Status extends Component {
       <PageContent title="Status">
         <PageContentBody>
           <HealthConnector />
-          <DiskSpaceConnector />
           <AboutConnector />
           <MoreInfo />
         </PageContentBody>
